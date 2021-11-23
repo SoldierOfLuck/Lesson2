3c18b05957d9e7c0168c461c5cb18a02a9afa94d
Artur Lukmanov <soldierofluck@gmail.com>
2021-11-23 12:43:06 +0300

    По заданию 4

--- app/src/main/java/ru/lukmanov/homework/MainActivity.java
+++ app/src/main/java/ru/lukmanov/homework/MainActivity.java
@@ -137,7 +137,6 @@ public class MainActivity extends AppCompatActivity {
                 flag_DOT=false;
             }
         });
-
         buttonSubtract.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
